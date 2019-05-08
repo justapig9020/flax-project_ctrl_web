fdf5974f9adc35a7182f5fc6cf1a6247c3670636
justapig9020 <justapig9020@gmail.com>
2019-05-08 14:02:33 +0800

    include update: get_pid

--- include/sql.php
+++ include/sql.php
@@ -9,4 +9,86 @@
 		} 	
 		return $db;
 	}
+	function get_pid ($pname,$oid)
+	{
+		$db = str_con();
+		if ($db){
+			//echo "連線成功</br>";
+			$sel = "select project.id as id from project inner join do_proj on id=project_id where name=:pname and user_id=:oid and status=0";
+			try {
+				$ins = $db->prepare($sel); 
+				if($ins){
+					$ins->bindParam(':oid',$oid);
+					$ins->bindParam(':pname',$pname);
+					$result = $ins->execute();
+					if($result){
+							$row = $ins->fetchall(PDO::FETCH_ASSOC);
+					}else{
+						$error = $ins->errorInfo();
+						//echo "查詢失敗".$error[2];
+					}
+				}
+			} catch (PDOException $e){
+				
+			}
+			if(!$row){
+				$retM = "無此專案";
+			}else{
+				//echo count($row);
+				//$row as $key=>$value;
+				$retV = $row[0]["id"];
+			}
+		$result=null;
+		$row=null;
+		$db=null;
+		}
+		return $retV;
+	}
+?>
+<?php
+    include("dbconf.php");
+	function str_con () {
+		try {
+			$db = new PDO('mysql:host=localhost;dbname=project_ctrl;charsetutf8',getDBUser(),getDBPw());
+		} catch (\PDOException $e) {
+			//echo "Error: ".$e."</br>";
+			return null;
+		} 	
+		return $db;
+	}
+	function get_pid ($pname,$oid)
+	{
+		$db = str_con();
+		if ($db){
+			//echo "連線成功</br>";
+			$sel = "select project.id as id from project inner join do_proj on id=project_id where name=:pname and user_id=:oid and status=0";
+			try {
+				$ins = $db->prepare($sel); 
+				if($ins){
+					$ins->bindParam(':oid',$oid);
+					$ins->bindParam(':pname',$pname);
+					$result = $ins->execute();
+					if($result){
+							$row = $ins->fetchall(PDO::FETCH_ASSOC);
+					}else{
+						$error = $ins->errorInfo();
+						//echo "查詢失敗".$error[2];
+					}
+				}
+			} catch (PDOException $e){
+				
+			}
+			if(!$row){
+				$retM = "無此專案";
+			}else{
+				//echo count($row);
+				//$row as $key=>$value;
+				$retV = $row[0]["id"];
+			}
+		$result=null;
+		$row=null;
+		$db=null;
+		}
+		return $retV;
+	}
 ?>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+include("include/sql.php");
+$id = get_pid("prj1",40543213);
+echo $id;
+?>
