92af276c291843465c75cc949a44a77257478e1e
ou <sun021988@gmail.com>
2019-06-16 22:58:26 +0800

    完成403排版

--- main/nonono.php
+++ main/nonono.php
@@ -1,5 +1,19 @@
 <script>
 setTimeout(function(){document.location.href="index.php";},3000);
 </script>
-阿捏母湯喔
-<img src="image/mutan.png">
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-2"></div>
+    
+        <div class="col-8 text-center">
+            <h1>阿捏母湯喔</h1>
+
+            <img src="image/mutan.png" class="img-fluid">
+        </div>
+    
+        <div class="col-2"></div>
+    </div>
+</div>
